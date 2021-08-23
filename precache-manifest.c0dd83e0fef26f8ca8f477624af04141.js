e516c460c8b37a7218c5363650910f6edd42a862
Ricardo Rodriguez <ricardoa.rodriguez@campusucc.edu.co>
2021-08-23 07:57:32 -0500

    Updates

---
+++ precache-manifest.c0dd83e0fef26f8ca8f477624af04141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad75a39aab4f4fe82477847daaf9df48",
+    "url": "/react-gifexpertapp/index.html"
+  },
+  {
+    "revision": "7f6f4596cc39d7d01526",
+    "url": "/react-gifexpertapp/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/react-gifexpertapp/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gifexpertapp/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6f4596cc39d7d01526",
+    "url": "/react-gifexpertapp/static/js/main.55ea919e.chunk.js"
+  },
+  {
+    "revision": "458b80fade44981dc42b",
+    "url": "/react-gifexpertapp/static/js/runtime-main.32f2a6a4.js"
+  }
+]);
