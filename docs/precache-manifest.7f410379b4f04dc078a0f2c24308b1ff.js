381f8c925ffc190ea38e736ea909218bb52b16ae
Ricardo Rodriguez <ricardoa.rodriguez@campusucc.edu.co>
2021-08-23 07:47:16 -0500

    docs agregado

---
+++ docs/precache-manifest.7f410379b4f04dc078a0f2c24308b1ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5544381c662298430425182eacd73228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f6f4596cc39d7d01526",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6f4596cc39d7d01526",
+    "url": "/static/js/main.55ea919e.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
